b1ce98810dce7dc90595ac80b1b62e9f2ea250e2
jurapro <jurapro@yandex.ru>
2021-03-29 13:02:39 +0700

    refactoring code

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api-cafe/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api-cafe/*'],
 
     'allowed_methods' => ['*'],
 
@@ -25,10 +25,10 @@ return [
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['*'],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
